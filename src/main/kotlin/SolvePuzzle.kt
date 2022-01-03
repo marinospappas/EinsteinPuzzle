2a789a03e4189fb61fecc22535effa060b8ab49f
marinospappas <92875023+marinospappas@users.noreply.github.com>
2022-01-03 08:27:48 +0100

    added debug info-1

--- src/main/kotlin/SolvePuzzle.kt
+++ src/main/kotlin/SolvePuzzle.kt
@@ -37,34 +37,33 @@ fun cloneHouses(houses: Array<House>): Array<House> {
 
 fun solveIt(rules: Array<Rule>, houses: Array<House>, ruleIndx: Int, debug: Boolean = false): Array<House>? {
     var h = cloneHouses(houses)
-    if (debug)
-        println("** trying rule $ruleIndx")
-    if (ruleIndx <= NUM_RULES) {
-        for (houseIndx in houses.indices) {
-            // apply next rule
-            h = cloneHouses(houses)
-            val result = rules[ruleIndx].rule(h, houseIndx)
-            if (!result)
-                continue    // could not apply rule - try next house
-            if (debug) {
-                println(">> rule $ruleIndx successfully applied for house $houseIndx")
-                print(">>")
-                printHouses(h)
-            }
-            val s = solveIt(rules, h, ruleIndx+1, debug)   // go and try to apply the next rule
-            if (s != null) {
-                return s    // good solution
-            }
-        }
-        ++count
-        if (debug)
-            println(">> rule $ruleIndx could not be applied")
-        return null   // run out of houses - cannot solve
-    } else {
+    if (ruleIndx > NUM_RULES) {
         if (debug)
             println(">> all rules applied - puzzle solved")
         return h
     }
+    if (debug)
+        println("** trying rule $ruleIndx")
+    for (houseIndx in houses.indices) {
+        // try to apply this rule
+        h = cloneHouses(houses)
+        val result = rules[ruleIndx].rule(h, houseIndx)
+        if (!result)
+            continue    // could not apply rule - try next house
+        if (debug) {
+            println(">> rule $ruleIndx successfully applied for house $houseIndx")
+            print(">>")
+            printHouses(h)
+        }
+        val s = solveIt(rules, h, ruleIndx + 1, debug)   // go and try to apply the next rule
+        if (s != null) {
+            return s    // good solution
+        }
+    }
+    ++count
+    if (debug)
+        println(">> rule $ruleIndx could not be applied")
+    return null   // run out of houses - cannot apply rule
 }
 
 fun main (args: Array<String>) {
